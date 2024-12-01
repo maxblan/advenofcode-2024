95b53ca79fabc9663cd6059d777da15218e19a3d
maxblan <35808469+maxblan@users.noreply.github.com>
2024-12-01 20:02:32 +0100

    feat: day1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string Path = "input";
+var lines = File.ReadAllLines(Path);
+
+List<int> array1 = [];
+List<int> array2 = [];
+
+for (var i = 0; i < lines.Length; i += 1)
+{
+    var line = lines[i];
+    var split = line.Split("   ");
+
+    if (split.Length < 2)
+    {
+        throw new Exception("Did not split correctly. Input was " + split);
+    }
+
+    if (int.TryParse(split[0], out int number1))
+    {
+        array1.Add(number1);
+    }
+    else
+    {
+        throw new Exception("Could not parse " + split[0]);
+    }
+
+    if (int.TryParse(split[1], out int number2))
+    {
+        array2.Add(number2);
+    }
+    else
+    {
+        throw new Exception("Could not parse " + split[1]);
+    }
+}
+
+if (array1.Count != array2.Count)
+{
+    throw new Exception("List lengths do not match.");
+}
+
+array1.Sort();
+array2.Sort();
+
+int sumOfDistances = 0;
+for (var i = 0; i < array1.Count; i += 1)
+{
+    sumOfDistances += Math.Abs(array1[i] - array2[i]);
+}
+
+Console.WriteLine(sumOfDistances);
+
+int similarityScore = 0;
+for (var i = 0; i < array1.Count; i += 1)
+{
+    similarityScore += array1[i] * array2.Count(number2 => number2 == array1[i]);
+}
+
+Console.WriteLine(similarityScore);
